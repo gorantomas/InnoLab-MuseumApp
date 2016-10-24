955358dcd284795707d2350946ed92e8679f3618
androidSDK <wepotec@gmail.com>
2016-10-24 18:18:41 +0200

    Startbildschirm mit Bild

--- app/src/main/java/li/uni/flmuseum/MainActivity.java
+++ app/src/main/java/li/uni/flmuseum/MainActivity.java
@@ -3,11 +3,29 @@ package li.uni.flmuseum;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import android.view.View;
+
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
 
    @Override protected void onCreate(Bundle savedInstanceState){
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
    }
+
+
+   public void checkFragen(View view){
+      Button button = (Button) findViewById(R.id.button);
+      button.setText("Hallo");
+     // button.invalidate();
+
+
+   }
+
+
+
 }
